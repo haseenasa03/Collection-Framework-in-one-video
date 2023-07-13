2d59a1fa20e1a306e12e298dd8dfe1b1f26db642
Haseena <haseenasa03@gmail.com>
2023-07-14 00:08:49 +0530

    first commit

---
+++ src/learn/StartCode.java
@@ -0,0 +1,24 @@
+package learn;
+import java.util.*;
+
+
+public class StartCode {
+    public static void main(String[] args) {
+        System.out.println("welcome to code java collection framework");
+
+
+        /* creating collection
+        1) Type Safe -same type elements(objects) are added to collection
+        2) Un type safe - different types of elements can be added to collection
+        */
+
+        //Type safe collection
+        ArrayList<String> names=new ArrayList<String>();
+        names.add("haseena");
+        System.out.println(names);
+
+
+        LinkedList list=new LinkedList();
+
+    }
+}
