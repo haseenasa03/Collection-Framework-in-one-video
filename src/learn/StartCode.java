9a2c237fa95cd80988265ba4b03dd7913b54eccf
Haseena <haseenasa03@gmail.com>
2023-07-14 16:08:55 +0530

    second commit1

--- src/learn/StartCode.java
+++ src/learn/StartCode.java
@@ -15,10 +15,27 @@ public class StartCode {
         //Type safe collection
         ArrayList<String> names=new ArrayList<String>();
         names.add("haseena");
+        names.add("sachin");
+        names.add("praveen");
+        names.add("nagraj");
+        names.add("rahul");
+        System.out.println(names);
+        System.out.println(names.get(4));
+        names.remove("haseena");
         System.out.println(names);
 
 
-        LinkedList list=new LinkedList();
+  //untype safe
+//        LinkedList list=new LinkedList();
+//        list.add("pramod");
+//        list.add("123");
+//        list.add("345.8");
+//        list.add("list");
+//        list.add("false");
+//        System.out.println(list);
+//        System.out.println(list.get(4));
+//
+
 
     }
 }
